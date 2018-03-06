25dcaba9a4d5658d302b3f65988a73f867bbb90e
Bosshammer, Benedikt <benedikt.bosshammer@sap.com>
2018-03-06 17:31:37 +0100

    required members for game scene

--- Cargonator/GameScene.swift
+++ Cargonator/GameScene.swift
@@ -10,6 +10,8 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
     
     override func sceneDidLoad() {
         
