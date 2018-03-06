0137a193d7c95c407404581d20338b1debe0409a
renpen <rene.penkert@gmail.com>
2018-03-06 23:11:34 +0100

    added circle

--- Cargonator/GameScene.swift
+++ Cargonator/GameScene.swift
@@ -21,5 +21,25 @@ class GameScene: SKScene {
         packageArea?.addChild(CGsquare)
         CGsquare.position = CGPoint(x: 10.0, y: 0.0)
         
+        
+        //circle
+        let CGcircle = SKShapeNode(circleOfRadius: 5)
+        CGcircle.lineWidth = 1
+        CGcircle.strokeColor = UIColor.red
+        CGcircle.yScale = packageAreaW / packageAreaH
+        CGcircle.fillColor = SKColor.red
+        CGcircle.zPosition = 0.1
+        packageArea?.addChild(CGcircle)
+        CGcircle.position = CGPoint(x: -10.0,y:0.0)
+        
+        //triangle
+        var points = [CGPoint(x:0.0,y:0.0),
+                      CGPoint(x:100.0,y:0.0),
+                      CGPoint(x:100.0,y:100.0)
+                      ]
+        var triangle = SKShapeNode(points: &points, count: points.count)
+        triangle.lineWidth = 10.0
+        triangle.strokeColor = UIColor.green
+        packageArea?.addChild(triangle)
     }
 }
