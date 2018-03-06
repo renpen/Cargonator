984e97851256b3b275e863fcdcc02bab234250b1
renpen <rene.penkert@gmail.com>
2018-03-06 17:15:07 +0100

    ADDED: Figure-enum/definition

---
+++ Cargonator/Figure.swift
@@ -0,0 +1,16 @@
+//
+//  Figure.swift
+//  Cargonator
+//
+//  Created by René on 06.03.18.
+//  Copyright © 2018 Cargonator Inc. All rights reserved.
+//
+
+import Foundation
+
+enum Figure {
+    case square
+    case triangle
+    case circle
+    case trapeze
+}
