908d2085df12398194963c25149969d705de1b89
benediktbosshammer <bosshammer.benedikt@student.dhbw-karlsruhe.de>
2018-03-19 16:13:30 +0100

    ADDED: Twitter login button + working constraints

--- Cargonator/AppDelegate.swift
+++ Cargonator/AppDelegate.swift
@@ -23,6 +23,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return TWTRTwitter.sharedInstance().application(app, open: url, options: options)
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Cargonator/Settings/SettingsViewController.swift
+++ Cargonator/Settings/SettingsViewController.swift
@@ -7,17 +7,41 @@
 //
 
 import UIKit
+import TwitterKit
 
 class SettingsViewController: UIViewController {
     
     var navigationDelegate: GameViewController?
     
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    override var prefersStatusBarHidden : Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let logInButton = TWTRLogInButton(logInCompletion: { session, error in
+            if (session != nil) {
+                print("signed in as \(session?.userName)");
+            } else {
+                print("error: \(error?.localizedDescription)");
+            }
+        })
+        
+        self.view.addSubview(logInButton)
+        
+        let horizontalConstraint = NSLayoutConstraint(item: logInButton, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: view, attribute: NSLayoutAttribute.centerX, multiplier: 1, constant: 0)
+        let widthConstraint = NSLayoutConstraint(item: logInButton, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: logInButton.frame.size.width)
+        let heightConstraint = NSLayoutConstraint(item: logInButton, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.notAnAttribute, multiplier: 1, constant: logInButton.frame.size.height)
+        let pinTop = NSLayoutConstraint(item: logInButton, attribute: .top, relatedBy: .equal,
+                                        toItem: navigationBar, attribute: .bottom, multiplier: 1.0, constant: 30)
+        
+        NSLayoutConstraint.activate([horizontalConstraint, widthConstraint, heightConstraint, pinTop])
+        logInButton.translatesAutoresizingMaskIntoConstraints = false;
     }
     
-    @IBAction func backPressed(_ sender: Any) {
+    @IBAction func donePressed(_ sender: Any) {
         self.navigationDelegate?.initMenuScene()
         self.dismiss(animated: false, completion: nil)
     }
